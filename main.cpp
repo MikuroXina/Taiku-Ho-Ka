9b24ce5aaec63fabe4fd3ae198fe6854dc7fb7ce
RkEclair <ryosukadnak@gmail.com>
2017-11-21 21:35:49 +0900

    Implement game system

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "MainController.hpp"
+#include "Data.hpp"
 
 int main() {
 	MainController mc{};
-	mc.init();
+	unsigned int highscore = 100;
+	mc.init(highscore);
 	while (mc.run() == 0) {
-		mc.init();
+		if (highscore < mc.getData()->score) {
+			highscore = mc.getData()->score;
+		}
+		mc.init(highscore);
 	}
 	return 0;
 }
